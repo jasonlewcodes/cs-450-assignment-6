4ad104cc93c6d9a72363227a28e9470f6b5fd319
jasonlewcodes <jiajie1224@gmail.com>
2025-04-06 00:24:23 -0400

    mostly working

--- cs-450-assignment-6/src/App.js
+++ cs-450-assignment-6/src/App.js
@@ -8,6 +8,12 @@ class App extends Component {
     super(props);
     this.state = {
       data: [],
+      selected: {
+        x: 300,
+        y: 300,
+        model: "",
+        index: 0,
+      },
     };
   }
   componentDidMount() {
@@ -20,9 +26,12 @@ class App extends Component {
     this.setState({ data: csv_data });
   };
   renderChart() {
+    const data = this.state.data;
+    if (!data.length) {
+      return;
+    }
     const width = 400;
     const height = 400;
-    const data = this.state.data;
     const maxSum = d3.sum([
       d3.max(data, (d) => d["GPT-4"]),
       d3.max(data, (d) => d["Gemini"]),
@@ -41,19 +50,38 @@ class App extends Component {
     var stack = d3.stack().keys(keys).offset(d3.stackOffsetWiggle);
     var stackedSeries = stack(data);
 
+    // area
     var areaGenerator = d3
       .area()
       .x((d) => xScale(d.data.Date))
       .y0((d) => yScale(d[0]))
       .y1((d) => yScale(d[1]))
       .curve(d3.curveCardinal);
-
     d3.select(".container")
       .selectAll("path")
       .data(stackedSeries)
       .join("path")
       .style("fill", (d, i) => colors[i])
-      .attr("d", (d) => areaGenerator(d));
+      .attr("d", (d) => areaGenerator(d))
+      .attr("model", (d) => d.key)
+      .attr("index", (d) => d.index)
+      .on("mousemove", (e, d) => {
+        this.setState({
+          selected: {
+            x: e.x,
+            y: e.y,
+            model: d.key,
+            index: d.index,
+          },
+        });
+      })
+      .on("mouseout", () => {
+        this.setState({
+          selected: {
+            model: "",
+          },
+        });
+      });
 
     // x axis
     var xAxisGenerator = d3
@@ -88,6 +116,66 @@ class App extends Component {
       .attr("y", (d) => bandScale(d) + 20)
       .attr("text-anchor", "start")
       .text((d) => d);
+
+    // bar chart
+    if (!this.state.selected.model) {
+      d3.select(".tooltip").attr("visibility", "hidden");
+      return;
+    }
+    d3.select(".tooltip")
+      .attr(
+        "transform",
+        `translate(${this.state.selected.x - 100}, ${
+          this.state.selected.y - 110
+        })`
+      )
+      .attr("visibility", "visible");
+    d3.select(".background")
+      .selectAll("rect")
+      .data([null])
+      .join("rect")
+      .attr("width", 240)
+      .attr("height", 120)
+      .attr("fill", "lightgrey");
+    const left = 32;
+    const top = 20;
+    const barChartWidth = 200;
+    const barChartHeight = 100;
+    const yMax = d3.max(data, (d) => d[this.state.selected.model]);
+    const barXScale = d3
+      .scaleBand()
+      .domain(data.map((d) => d.Date))
+      .range([left, left + barChartWidth])
+      .padding(0.1);
+    const barYScale = d3
+      .scaleLinear()
+      .domain([0, yMax])
+      .range([barChartHeight, top]);
+    const barXAxisGenerator = d3
+      .axisBottom(barXScale)
+      .tickFormat((d) =>
+        d.toLocaleString("default", { month: "long" }).substring(0, 3)
+      );
+    const barYAxisGenerator = d3.axisLeft(barYScale);
+
+    d3.select(".bar-chart-x-axis")
+      .attr("transform", `translate(0, ${barChartHeight})`)
+      .call(barXAxisGenerator);
+    d3.select(".bar-chart-y-axis")
+      .attr("transform", `translate(${left}, 0)`)
+      .call(barYAxisGenerator);
+    d3.select(".bars")
+      .selectAll("rect")
+      .data(data)
+      .join("rect")
+      .attr("x", (d) => barXScale(d.Date))
+      .attr("y", (d) => barYScale(d[this.state.selected.model]))
+      .attr("width", barXScale.bandwidth())
+      .attr(
+        "height",
+        (d) => barYScale(yMax - d[this.state.selected.model]) - 20
+      )
+      .attr("fill", colors[this.state.selected.index]);
   }
   render() {
     return (
@@ -97,6 +185,12 @@ class App extends Component {
           <g className="container"></g>
           <g className="x-axis"></g>
           <g className="legend"></g>
+          <g className="tooltip">
+            <g className="background"></g>
+            <g className="bar-chart-x-axis"></g>
+            <g className="bar-chart-y-axis"></g>
+            <g className="bars"></g>
+          </g>
         </svg>
       </div>
     );
